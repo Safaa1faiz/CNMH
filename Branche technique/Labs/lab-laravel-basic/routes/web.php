be5c2fd3ff54b5392059e1babe5f6e988844742c
Safaâ Faiz <88689147+Safaa1faiz@users.noreply.github.com>
2023-11-21 15:37:29 +0100

    .

--- Branche technique/Labs/lab-laravel-basic/routes/web.php
+++ Branche technique/Labs/lab-laravel-basic/routes/web.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\TaskController;
+
+// Route::prefix('tasks')->group(function () {
+    // Index page to show all tasks
+// routes/web.php
+
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/', [TaskController::class, 'store'])->name('tasks.store');
+Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+Route::post('/{task}', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+
+// });
